2754f9076c45ae056abf1b97efaf95de48c0bf2f
Sukhanth Nandam <96712372+SukhanthN@users.noreply.github.com>
2023-05-13 22:32:39 +0530

    modified the error in 15 line

--- src/main/java/com/javatechie/DevopsIntegrationApplication.java
+++ src/main/java/com/javatechie/DevopsIntegrationApplication.java
@@ -12,7 +12,7 @@ public class DevopsIntegrationApplication {
 	@GetMapping
 	public String message() {
 		//String styledMessage = "<div style=\"font-weight: bold; background-color: red;\">Hello world this sukhanth</div>";
-		String styledMessage = "<html><head><title>Hello World</title><style>html,body {height:100%;margin:0;}head {display:flex;align-items:center;justify-content:center;flex-direction:column;}</style></head><body style='background-color:lightblue;padding:20px'><h1 style='color:green;text-align:center'>Hello World! This is Sukhanth</h1><h2 style='color:#00008B;text-align:center'>This is Jenkins CI CD Pipeline Java SpringBoot Application <span style='color:red'>Version:4</span></h2></body></html>"		
+		String styledMessage = "<html><head><title>Hello World</title><style>html,body {height:100%;margin:0;}head {display:flex;align-items:center;justify-content:center;flex-direction:column;}</style></head><body style='background-color:lightblue;padding:20px'><h1 style='color:green;text-align:center'>Hello World! This is Sukhanth</h1><h2 style='color:#00008B;text-align:center'>This is Jenkins CI CD Pipeline Java SpringBoot Application <span style='color:red'>Version:4</span></h2></body></html>;"		
     		return styledMessage;
 }
 
