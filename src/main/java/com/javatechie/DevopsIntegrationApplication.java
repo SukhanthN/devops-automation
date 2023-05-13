5fce4251944e85dfb8e78f7f463287be6ee4db13
Sukhanth Nandam <96712372+SukhanthN@users.noreply.github.com>
2023-05-13 17:42:02 +0530

    Update DevopsIntegrationApplication.java add calour

--- src/main/java/com/javatechie/DevopsIntegrationApplication.java
+++ src/main/java/com/javatechie/DevopsIntegrationApplication.java
@@ -10,9 +10,11 @@ import org.springframework.web.bind.annotation.RestController;
 public class DevopsIntegrationApplication {
 
 	@GetMapping
-	public String message(){
-		return "Hello world this sukhanth";
-	}
+	public String message() {
+	    String boldText = "\u001B[1mHello world this sukhanth\u001B[0m";
+	    String coloredBackground = "\u001B[43m" + boldText + "\u001B[0m";
+	    return coloredBackground;
+}
 
 	public static void main(String[] args) {
 		SpringApplication.run(DevopsIntegrationApplication.class, args);
